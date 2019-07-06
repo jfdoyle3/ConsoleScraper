7fb5c36c01635b0461fb8046133582e56455ecc1
Jim Doyle <indellibleink@gmail.com>
2019-07-06 10:26:55 -0400

    Added Scraper project

---
+++ ConsoleScraper/FromFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+using System.IO;
+using System.Linq;
+using System.Data;
+
+
+namespace ConsoleScraper
+{
+    public class FromFile
+    {
+        public List<List<string>> ReadFile()
+        {
+            string stocks = File.ReadAllText(@"C:\website\Stocks.htm");
+            HtmlDocument htmlFile = new HtmlDocument();
+
+            htmlFile.LoadHtml(stocks);
+
+
+            List<List<string>> stockTable =
+                                    htmlFile.DocumentNode.SelectSingleNode("//table")
+                                                .Descendants("tr")
+                                                .Skip(1) //To Skip Table Header Row
+                                                .Where(tr => tr.Elements("td").Count() > 1)
+                                                .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                                                .ToList();
+
+            return stockTable;
+        }
+    }
+}
---
+++ ConsoleScraper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FromFile scrape = new FromFile();
+            // List<List<string>> stockTable = scrape.ReadFile();
+
+            YahooFinance scrape = new YahooFinance();
+            List<List<string>> stockTable = scrape.Login();
+
+            FinanceTable.ScrapeToDatabase(stockTable);
+        }
+    }
+}
