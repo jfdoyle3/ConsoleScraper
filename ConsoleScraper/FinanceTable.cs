01c36c6d81e99f3b2847b0efff1786d7f55f55c3
Jim Doyle <indellibleink@gmail.com>
2019-07-31 01:58:12 -0400

    add new field and removed a field

--- ConsoleScraper/FinanceTable.cs
+++ ConsoleScraper/FinanceTable.cs
@@ -11,24 +11,25 @@ namespace ConsoleScraper
     {
         public static void ScrapeToDatabase(List<List<string>> stockData)
         {
+            string method = "HAP";
             using (StocksContext db = new StocksContext())
             {
                 
                 for (int i = 0; i < stockData.Count; i++)
                 {
-                    
+
                     StockTable stockTable = new StockTable
                     {
-                        DateStamp=DateTime.Now,
+                        DateStamp = DateTime.Now,
                         Symbol = stockData[i][0].ToString(),
                         LastPrice = stockData[i][1].ToString(),
                         Change = stockData[i][2].ToString(),
                         ChgPc = stockData[i][3].ToString(),
-                        Currency = stockData[i][4].ToString(),
                         MarketTime = stockData[i][5].ToString(),
                         Volume = stockData[i][6].ToString(),
                         AvgVol3m = stockData[i][8].ToString(),
                         MarketCap = stockData[i][12].ToString(),
+                        Method=method,
                     };
 
                     db.StockTables.Add(stockTable);
--- ConsoleScraper/Program.cs
+++ ConsoleScraper/Program.cs
@@ -16,6 +16,14 @@ namespace ConsoleScraper
             YahooFinance scrape = new YahooFinance();
             List<List<string>> stockTable = scrape.Login();
 
+            for (var i = 0; i < stockTable.Count; i++)
+            {
+                for (int j = 0; j < stockTable[0].Count; j++)
+                {
+                    Console.WriteLine(stockTable[i][j].ToString());
+                }
+            }
+            //
             FinanceTable.ScrapeToDatabase(stockTable);
         }
     }
